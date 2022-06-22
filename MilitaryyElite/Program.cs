0aaaef031523bc0fd339c93e41d51b07c258fe7f
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2022-06-22 17:08:37 +0300

    <Logic input in progress>

--- MilitaryyElite/Program.cs
+++ MilitaryyElite/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MilitaryyElite.Implementations;
+using MilitaryyElite.Interfaces;
+using System;
+using System.Collections.Generic;
 
 namespace MilitaryyElite
 {
@@ -7,6 +10,8 @@ namespace MilitaryyElite
         public static void Main(string[] args)
         {
             string input = Console.ReadLine();
+
+            Dictionary<int, ISoldier> soldiers = new Dictionary<int, ISoldier>();
 
             while (input != "End")
             {
@@ -17,6 +22,24 @@ namespace MilitaryyElite
                 int id = int.Parse(inputInfo[1]);
                 string firstName = inputInfo[2];
                 string lastName = inputInfo[3];
+
+                if (soldierType == "Private" )
+                {
+                    decimal salary = decimal.Parse(inputInfo[4]);
+                    IPrivate @private = new Private(id, firstName,
+                        lastName, salary);
+                    soldiers.Add(id, @private);
+                }
+                else if (soldierType == "LeutenantGeneral")
+                {
+                    decimal salary = decimal.Parse(inputInfo[4]);
+                    ILieutenantGeneral leutenantGeneral = new LieutenantGeneral
+                        (id, firstName, lastName, salary);
+                }
+
+
+
+                input = Console.ReadLine();
             }
         }
     }
